9ca353762a7efdb6ee1f46d5520574af29537d12
olijeffers0n <69084614+olijeffers0n@users.noreply.github.com>
2024-11-24 20:01:51 +0000

    Add initial GET implementation to backend

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,33 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	// Your code here
+	http.HandleFunc("/", ToDoListHandler)
+	fmt.Println("Server is running on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		// Handle error
+		fmt.Println("Error starting server: ", err)
+	}
 }
 
 func ToDoListHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
 
-	// Your code here
+	switch r.Method {
+	case http.MethodGet:
+		// Return the list of todos
+		w.WriteHeader(http.StatusOK)
+		err := json.NewEncoder(w).Encode([]string{})
+		if err != nil {
+			// Handle error
+			fmt.Println("Error encoding response: ", err)
+		}
+	}
 }
